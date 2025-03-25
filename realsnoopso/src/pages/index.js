379d78f10359b074aeabeb02e4d711971ff53e9f
realsnoopso <real@snoop.so>
2025-03-25 12:33:25 +0900

    Refactor index page to redirect to external URL and remove blog content

--- realsnoopso/src/pages/index.js
+++ realsnoopso/src/pages/index.js
@@ -1,94 +1,107 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+// import * as React from "react"
+// import { Link, graphql } from "gatsby"
+// import Layout from "../components/layout"
+// import Seo from "../components/seo"
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+// const BlogIndex = ({ data, location }) => {
+//   const siteTitle = data.site.siteMetadata?.title || `Title`
+//   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
+//   if (posts.length === 0) {
+//     return (
+//       <Layout location={location} title={siteTitle}>
+//         <p>
+//           No blog posts found. Add markdown posts to "content/blog" (or the
+//           directory you specified for the "gatsby-source-filesystem" plugin in
+//           gatsby-config.js).
+//         </p>
+//       </Layout>
+//     )
+//   }
 
-  return (
-    <Layout location={location} title={siteTitle}>
-      <ol
-        style={{
-          listStyle: `none`,
-          paddingLeft: "var(--spacing-0)",
-          margin: 0,
-        }}
-      >
-        {posts.map((post, i) => {
-          const title = post.frontmatter.title || post.fields.slug
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <h2 style={{ marginTop: i === 0 ? 0 : "auto" }}>
-                  <Link to={post.fields.slug} itemProp="url">
-                    <span itemProp="headline">{title}</span>
-                  </Link>
-                </h2>
-                <small style={{ color: "var(--color-text-light)" }}>
-                  {post.frontmatter.date}
-                </small>
-                {/* <p
-                  style={{ marginTop: "var(--spacing-1)" }}
-                  dangerouslySetInnerHTML={{
-                    __html: post.frontmatter.description || post.excerpt,
-                  }}
-                  itemProp="description"
-                /> */}
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-    </Layout>
-  )
+//   return (
+//     <Layout location={location} title={siteTitle}>
+//       <ol
+//         style={{
+//           listStyle: `none`,
+//           paddingLeft: "var(--spacing-0)",
+//           margin: 0,
+//         }}
+//       >
+//         {posts.map((post, i) => {
+//           const title = post.frontmatter.title || post.fields.slug
+//           return (
+//             <li key={post.fields.slug}>
+//               <article
+//                 className="post-list-item"
+//                 itemScope
+//                 itemType="http://schema.org/Article"
+//               >
+//                 <h2 style={{ marginTop: i === 0 ? 0 : "auto" }}>
+//                   <Link to={post.fields.slug} itemProp="url">
+//                     <span itemProp="headline">{title}</span>
+//                   </Link>
+//                 </h2>
+//                 <small style={{ color: "var(--color-text-light)" }}>
+//                   {post.frontmatter.date}
+//                 </small>
+//                 {/* <p
+//                   style={{ marginTop: "var(--spacing-1)" }}
+//                   dangerouslySetInnerHTML={{
+//                     __html: post.frontmatter.description || post.excerpt,
+//                   }}
+//                   itemProp="description"
+//                 /> */}
+//               </article>
+//             </li>
+//           )
+//         })}
+//       </ol>
+//     </Layout>
+//   )
+// }
+
+// export default BlogIndex
+
+// /**
+//  * Head export to define metadata for the page
+//  *
+//  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+//  */
+// export const Head = () => <Seo title="All posts" />
+
+// export const pageQuery = graphql`
+//   {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+//       nodes {
+//         excerpt
+//         fields {
+//           slug
+//         }
+//         frontmatter {
+//           date(formatString: "MMMM DD, YYYY")
+//           title
+//           description
+//         }
+//       }
+//     }
+//   }
+// `
+
+import { navigate } from "gatsby"
+import { useEffect } from "react"
+
+const IndexPage = () => {
+  useEffect(() => {
+    navigate("https://frequent-reassurance-292682.framer.app/") // 여기에 원하시는 리다이렉션 주소를 넣어주세요
+  }, [])
+
+  return null
 }
 
-export default BlogIndex
-
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="All posts" />
-
-export const pageQuery = graphql`
-  {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
-      }
-    }
-  }
-`
+export default IndexPage
